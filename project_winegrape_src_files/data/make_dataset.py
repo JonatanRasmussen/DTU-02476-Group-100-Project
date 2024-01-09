44e86ef6198d0f9b5029a073ef72f777319d9afa
Oshkr <oskar.h.kristoffersen@gmail.com>
2024-01-09 15:52:43 +0100

    Downloading data from Kaggle, Implementation of Data and Training files with timm and pytorch lightning

--- project_winegrape_src_files/data/make_dataset.py
+++ project_winegrape_src_files/data/make_dataset.py
@@ -1,3 +1,30 @@
-if __name__ == '__main__':
-    # Get the data and process it
-    pass
+import logging
+import os
+import zipfile
+from pathlib import Path
+
+# from dotenv import find_dotenv, load_dotenv
+import opendatasets as od
+
+
+def main():
+    """Downloads the data from Kaggle and places it in "data/raw/"
+    """
+
+    dataset_path = os.path.join(os.getcwd(),"data","raw")
+    try:
+        import kaggle  # type: ignore
+    except Exception:
+        print(
+            "Must athenticate the kaggle api according to https://www.kaggle.com/docs/api"  # noqa: E501
+        )
+        exit(1)
+
+    dataset = 'https://www.kaggle.com/datasets/muratkokludataset/grapevine-leaves-image-dataset'
+    print("info: you can find your username and key under /.kaggle/kaggle.json")
+    
+    od.download_kaggle_dataset(dataset,dataset_path)
+
+
+if __name__ == "__main__":
+    main()
